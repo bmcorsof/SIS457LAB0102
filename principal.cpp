bb176027a220b13cdd8e5547536b947d3304e49d
bmcorsof <90282494+bmcorsof@users.noreply.github.com>
2022-04-05 00:25:44 -0400

    Update principal.cpp

---
+++ principal.cpp
@@ -0,0 +1,14 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+    cout << "Nombre Completo : Burlecot Matias Corso Flores" << endl;
+	cout << "Edad : 20" << endl;
+	cout << "Carrera : Ingenieria de Sistemas" << endl;
+	cout << "Altura : 1.89 metros" << endl;
+	cout << "Peso : 68 kg" << endl;
+	cout << "Cumpleaños : 15 de marzo" << endl;
+	cout << "Color favorito : Morado" << endl;
+	system("pause");
+	return 0;
+}
